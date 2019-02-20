8ea2776767afbf6ca3527b4fc7d2674668b83568
pierre <pm@enyopharma.com>
2019-02-20 16:51:29 +0100

    base layout

--- templates/index.php
+++ templates/index.php
@@ -1,2 +1,40 @@
 <?php $this->layout('layout'); ?>
-<h1>Hello world</h1>
+<div class="page-header">
+    <h1>
+        Welcome to drakkar!
+        <small class="d-none d-md-inline">Lets sail toward the vinland!</small>
+    </h1>
+</div>
+<div class="row">
+    <div class="col">
+        <a href="#" class="btn btn-primary btn-lg btn-block">
+            Download current vinland dataset
+        </a>
+    </div>
+</div>
+<div class="row">
+    <div class="col-4 d-none d-md-block">
+        <img src="<?= 'img/viking.png' ?>" class="img-fluid" style="width: 200px; opacity: 0.8;">
+    </div>
+    <div class="col offset-md-1">
+        <div class="card">
+            <h3 class="card-header">Ahoy mate, here is how to sail</h3>
+            <div class="card-body">
+                <ul>
+                    <li>
+                        Login to <strong>Drakkar</strong> with you ENYOpharma account
+                    </li>
+                    <li>
+                        Review publications from each curation run and discard the unrelevant ones
+                    </li>
+                    <li>
+                        Add descriptions for each relevant publication through the curation assistant form
+                    </li>
+                    <li>
+                        Any time you can download the current vinland dataset
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
--- templates/layout.php
+++ templates/layout.php
@@ -2,31 +2,11 @@
 <html>
     <head>
         <title>
-            <?= isset($title) ? $this->e($title) : 'Default title' ?>
+            <?= isset($title) ? $this->e($title) : 'Drakkar curation interface' ?>
         </title>
         <link rel="stylesheet" href="<?= $this->asset('build/app.css') ?>" />
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
         <div class="container">
             <div id="index"></div>
             <?= $this->section('content') ?>
