0bdcb4f4b0e11b8f1c159ef2d618d0af97b719ef
pierre <pm@enyopharma.com>
2019-03-07 14:39:40 +0100

    mark example command as final

--- app/Cli/Commands/ExampleCommand.php
+++ app/Cli/Commands/ExampleCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ExampleCommand extends Command
+final class ExampleCommand extends Command
 {
     protected static $defaultName = 'app:example';
 
