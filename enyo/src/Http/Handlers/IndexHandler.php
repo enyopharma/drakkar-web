2abdc5a5cf900cfd1c4dafcd340845695634a4c1
pierre <pm@enyopharma.com>
2019-03-19 11:43:04 +0100

    move default index handler under enyo namespace

--- enyo/src/Http/Handlers/IndexHandler.php
+++ enyo/src/Http/Handlers/IndexHandler.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Http\Handlers;
+namespace Enyo\Http\Handlers;
 
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
