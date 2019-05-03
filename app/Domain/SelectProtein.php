906dc5722c79768deaf761da2da7ed1cfca7f734
pierre <pm@enyopharma.com>
2019-05-03 09:55:05 +0200

    add sequence and interactors to protein

--- app/Domain/SelectProtein.php
+++ app/Domain/SelectProtein.php
@@ -7,9 +7,18 @@ final class SelectProtein
     const NOT_FOUND = 0;
 
     const SELECT_PROTEIN_SQL = <<<SQL
-        SELECT accession, name, description
-        FROM proteins
-        WHERE accession = ?
+        SELECT p.id, p.accession, p.name, p.description, s.sequence
+        FROM proteins AS p, sequences AS s
+        WHERE p.id = s.protein_id
+        AND s.is_canonical IS TRUE
+        AND p.accession = ?
+SQL;
+
+    const SELECT_INTERACTORS_SQL = <<<SQL
+        SELECT name, start, stop
+        FROM interactors
+        WHERE protein_id = ?
+        GROUP BY name, start, stop
 SQL;
 
     private $pdo;
@@ -22,10 +31,15 @@ SQL;
     public function __invoke(string $accession): DomainPayloadInterface
     {
         $select_protein_sth = $this->pdo->prepare(self::SELECT_PROTEIN_SQL);
+        $select_interactors_sth = $this->pdo->prepare(self::SELECT_INTERACTORS_SQL);
 
         $select_protein_sth->execute([$accession]);
 
         if ($protein = $select_protein_sth->fetch()) {
+            $select_interactors_sth->execute([$protein['id']]);
+
+            $protein['interactors'] = $select_interactors_sth->fetchall();
+
             return new DomainSuccess([
                 'protein' => $protein,
             ]);
