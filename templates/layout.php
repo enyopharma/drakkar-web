20d627b357c1e1177dc1263838e1794ea3e7f63d
pierre <pm@enyopharma.com>
2019-07-12 14:46:40 +0200

    fix bug with layout

--- templates/layout.php
+++ templates/layout.php
@@ -1,4 +1,4 @@
-<?php $this->layout('default', ['title' => $title ? $title : 'Drakkar curation interface']); ?>
+<?php $this->layout('default', ['title' => isset($title) ? $title : 'Drakkar curation interface']); ?>
 <div class="container">
     <?= $this->section('content') ?>
 </div>
