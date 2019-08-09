f5234396ce7d05ac93d455ad270281525596b9a1
pierre <pm@enyopharma.com>
2019-08-09 13:23:19 +0200

    better code organisation

--- config/factories/vendor.nyholm.psr7.php
+++ config/factories/vendor.nyholm.psr7.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use Psr\Http\Message\UriFactoryInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
-use Psr\Http\Message\UploadedFileFactoryInterface;
-use Psr\Http\Message\ServerRequestFactoryInterface;
 
 use Nyholm\Psr7\Factory\Psr17Factory;
 
@@ -24,14 +22,6 @@ return [
     },
 
     ResponseFactoryInterface::class => function ($container) {
-        return $container->get(Psr17Factory::class);
-    },
-
-    UploadedFileFactoryInterface::class => function ($container) {
-        return $container->get(Psr17Factory::class);
-    },
-
-    ServerRequestFactoryInterface::class => function ($container) {
         return $container->get(Psr17Factory::class);
     },
 ];
--- entrypoints/http.dev.php
+++ entrypoints/http.dev.php
@@ -15,11 +15,20 @@ $root = (string) realpath(__DIR__ . '/../');
 require $root . '/vendor/autoload.php';
 
 /**
+ * Register the exception handler.
+ */
+Quanta\Http\ExceptionHandler::register(true, true);
+
+/**
  * Get a http server in develoment environement with debug mode enabled.
  */
-$server = (require $root . '/http/server.php')($root, 'develoment', true);
+$server = new Quanta\Http\Server(
+    new App\Http\Server\NyholmContext(
+        require $root . '/http/app.php'
+    )
+);
 
 /**
  * Run the http server.
  */
-$server->run();
+$server->run($root, 'develoment', true);
