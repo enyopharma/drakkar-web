6bafcfec37e815fc4364d5872fca2431da443e16
pierre <pm@enyopharma.com>
2020-09-10 15:05:07 +0200

    better handling of existing descriptions

--- app/src/Actions/StoreDescriptionSql.php
+++ app/src/Actions/StoreDescriptionSql.php
@@ -8,10 +8,10 @@ use App\Input\DescriptionInput;
 
 final class StoreDescriptionSql implements StoreDescriptionInterface
 {
-    const SELECT_DESCRIPTION_SQL = <<<SQL
-        SELECT id
+    const SELECT_DESCRIPTIONS_SQL = <<<SQL
+        SELECT stable_id, mapping1, mapping2
         FROM descriptions
-        WHERE association_id = ? AND stable_id != ? AND method_id = ?
+        WHERE association_id = ? AND method_id = ?
         AND protein1_id = ? AND start1 = ? AND stop1 = ?
         AND protein2_id = ? AND start2 = ? AND stop2 = ?
         AND deleted_at IS NULL
@@ -56,7 +56,10 @@ final class StoreDescriptionSql implements StoreDescriptionInterface
         $interactor2 = $data['interactor2'];
 
         // ensure description does not exists.
-        if ($this->isExisting($association_id, $stable_id, $method_id, $interactor1, $interactor2)) {
+        $same1 = $this->isExisting($association_id, $stable_id, $method_id, $interactor1, $interactor2);
+        $same2 = $this->isExisting($association_id, $stable_id, $method_id, $interactor2, $interactor1);
+
+        if ($same1 || $same2) {
             return StoreDescriptionResult::descriptionAlreadyExists();
         }
 
@@ -68,11 +71,10 @@ final class StoreDescriptionSql implements StoreDescriptionInterface
 
     private function isExisting(int $association_id, string $stable_id, int $method_id, array $interactor1, array $interactor2): bool
     {
-        $select_description_sth = $this->pdo->prepare(self::SELECT_DESCRIPTION_SQL);
+        $select_descriptions_sth = $this->pdo->prepare(self::SELECT_DESCRIPTIONS_SQL);
 
-        $select_description_sth->execute([
+        $select_descriptions_sth->execute([
             $association_id,
-            $stable_id,
             $method_id,
             $interactor1['protein_id'],
             $interactor1['start'],
@@ -82,24 +84,17 @@ final class StoreDescriptionSql implements StoreDescriptionInterface
             $interactor2['stop'],
         ]);
 
-        if ($select_description_sth->fetch()) {
-            return true;
-        }
+        while ($description = $select_descriptions_sth->fetch()) {
+            if ($description['stable_id'] != $stable_id) {
+                return true;
+            }
 
-        $select_description_sth->execute([
-            $association_id,
-            $stable_id,
-            $method_id,
-            $interactor2['protein_id'],
-            $interactor2['start'],
-            $interactor2['stop'],
-            $interactor1['protein_id'],
-            $interactor1['start'],
-            $interactor1['stop'],
-        ]);
+            $same1 = $this->sameMapping(json_decode($description['mapping1'], true), $interactor1['mapping']);
+            $same2 = $this->sameMapping(json_decode($description['mapping2'], true), $interactor2['mapping']);
 
-        if ($select_description_sth->fetch()) {
-            return true;
+            if ($same1 && $same2) {
+                return true;
+            }
         }
 
         return false;
@@ -198,5 +193,45 @@ final class StoreDescriptionSql implements StoreDescriptionInterface
         $description['id'] = (int) $this->pdo->lastInsertId();
 
         return StoreDescriptionResult::success($description);
+    }
+
+    private function sameMapping(array $mapping1, array $mapping2): bool
+    {
+        $flat1 = iterator_to_array($this->flatAlignments($mapping1));
+        $flat2 = iterator_to_array($this->flatAlignments($mapping2));
+
+        if (count($flat1) != count($flat2)) return false;
+
+        foreach ($flat1 as $hash => $isoforms) {
+            if (!array_key_exists($hash, $flat2)) return false;
+
+            if (count($isoforms) != count($flat2[$hash])) return false;
+
+            foreach ($isoforms as $accession => $starts) {
+                if (!array_key_exists($accession, $flat2[$hash])) return false;
+
+                if (count($starts) != count($flat2[$hash][$accession])) return false;
+
+                foreach ($starts as $start) {
+                    if (!in_array($start, $flat2[$hash][$accession])) return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private function flatAlignments(array $alignments): \Generator
+    {
+        foreach ($alignments as ['sequence' => $sequence, 'isoforms' => $isoforms]) {
+            yield md5($sequence) => iterator_to_array($this->flatIsoforms($isoforms));
+        }
+    }
+
+    private function flatIsoforms(array $isoforms): \Generator
+    {
+        foreach ($isoforms as ['accession' => $accession, 'occurrences' => $occurrences]) {
+            yield $accession => array_map(fn (array $o) => $o['start'], $occurrences);
+        }
     }
 }
