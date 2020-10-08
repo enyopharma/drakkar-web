53587d3b106d045d00fe139ce389645a2a78f4f6
pierre <pm@enyopharma.com>
2020-10-08 15:20:05 +0200

    better lazy request handler factory

--- app/config/routes.php
+++ app/config/routes.php
@@ -19,98 +19,113 @@ use App\Endpoints\Descriptions;
  * @return array[]
  */
 return function (ContainerInterface $container): array {
-    $responder = new Quanta\Http\Responder(
-        $container->get(Psr\Http\Message\ResponseFactoryInterface::class),
-    );
+    $lazy = function (callable $factory) use ($container) {
+        return new Quanta\Http\LazyRequestHandler(function () use ($container, $factory) {
+            $xs = $factory();
 
-    $lazy = fn (callable $f) => new Quanta\Http\LazyRequestHandler($f);
-    $endpoint = fn (callable $f) => new Quanta\Http\Endpoint($responder, $f);
+            $responder = new Quanta\Http\Responder(
+                $container->get(Psr\Http\Message\ResponseFactoryInterface::class),
+            );
+
+            [$f, $middleware] = is_array($xs) ? [array_shift($xs), $xs] : [$xs, []];
+
+            if (!is_callable($f)) {
+                throw new \LogicException('invalid endpoint');
+            }
+
+            $endpoint = new Quanta\Http\Endpoint($responder, $f);
+
+            return count($middleware) > 0
+                ? Quanta\Http\RequestHandler::queue($endpoint, ...$middleware)
+                : $endpoint;
+        });
+    };
 
     return array_map($lazy, [
-        'GET /' => fn () => $endpoint(new Runs\IndexEndpoint(
+        'GET /' => fn () => new Runs\IndexEndpoint(
             $container->get(League\Plates\Engine::class),
             $container->get(App\ReadModel\RunViewInterface::class),
-        )),
+        ),
 
-        'GET /publications' => fn () => $endpoint(new Publications\SearchEndpoint(
+        'GET /publications' => fn () => new Publications\SearchEndpoint(
             $container->get(League\Plates\Engine::class),
             $container->get(App\ReadModel\PublicationViewInterface::class),
-        )),
+        ),
 
-        'GET /descriptions' => fn () => $endpoint(new Descriptions\SearchEndpoint(
+        'GET /descriptions' => fn () => new Descriptions\SearchEndpoint(
             $container->get(App\Routing\UrlGenerator::class),
             $container->get(League\Plates\Engine::class),
             $container->get(App\ReadModel\DescriptionViewInterface::class),
-        )),
+        ),
 
-        'GET /runs/{run_id:\d+}/publications' => fn () => $endpoint(new Publications\IndexEndpoint(
+        'GET /runs/{run_id:\d+}/publications' => fn () => new Publications\IndexEndpoint(
             $container->get(League\Plates\Engine::class),
             $container->get(App\Routing\UrlGenerator::class),
             $container->get(App\ReadModel\RunViewInterface::class),
             $container->get(App\ReadModel\AssociationViewInterface::class),
-        )),
+        ),
 
-        'PUT /runs/{run_id:\d+}/publications/{pmid:\d+}' => fn () => $endpoint(new Publications\UpdateEndpoint(
+        'PUT /runs/{run_id:\d+}/publications/{pmid:\d+}' => fn () => new Publications\UpdateEndpoint(
             $container->get(App\Actions\UpdatePublicationStateInterface::class),
-        )),
+        ),
 
-        'GET /runs/{run_id:\d+}/publications/{pmid:\d+}/descriptions' => fn () => $endpoint(new Descriptions\IndexEndpoint(
+        'GET /runs/{run_id:\d+}/publications/{pmid:\d+}/descriptions' => fn () => new Descriptions\IndexEndpoint(
             $container->get(League\Plates\Engine::class),
             $container->get(App\Routing\UrlGenerator::class),
             $container->get(App\ReadModel\RunViewInterface::class),
             $container->get(App\ReadModel\AssociationViewInterface::class),
             $container->get(App\ReadModel\DescriptionViewInterface::class),
-        )),
+        ),
 
-        'GET /runs/{run_id:\d+}/publications/{pmid:\d+}/descriptions/create' => fn () => $endpoint(new Descriptions\CreateEndpoint(
+        'GET /runs/{run_id:\d+}/publications/{pmid:\d+}/descriptions/create' => fn () => new Descriptions\CreateEndpoint(
             $container->get(League\Plates\Engine::class),
             $container->get(App\ReadModel\RunViewInterface::class),
             $container->get(App\ReadModel\AssociationViewInterface::class),
-        )),
+        ),
 
-        'GET /runs/{run_id:\d+}/publications/{pmid:\d+}/descriptions/{id:\d+}/{type:copy|edit}' => fn () => $endpoint(new Descriptions\EditEndpoint(
+        'GET /runs/{run_id:\d+}/publications/{pmid:\d+}/descriptions/{id:\d+}/{type:copy|edit}' => fn () => new Descriptions\EditEndpoint(
             $container->get(League\Plates\Engine::class),
             $container->get(App\ReadModel\RunViewInterface::class),
             $container->get(App\ReadModel\AssociationViewInterface::class),
             $container->get(App\ReadModel\FormViewInterface::class),
-        )),
+        ),
 
-        'POST /runs/{run_id:\d+}/publications/{pmid:\d+}/descriptions' => fn () => Quanta\Http\RequestHandler::queue(
-            $endpoint(new Descriptions\StoreEndpoint(
+        'POST /runs/{run_id:\d+}/publications/{pmid:\d+}/descriptions' => fn () => [
+            new Descriptions\StoreEndpoint(
                 $container->get(App\Actions\StoreDescriptionInterface::class),
-            )),
+            ),
             new App\Middleware\ValidateDescriptionMiddleware(
                 $container->get(PDO::class),
                 $container->get(Psr\Http\Message\ResponseFactoryInterface::class),
             ),
+        ],
+
+        'DELETE /runs/{run_id:\d+}/publications/{pmid:\d+}/descriptions/{id:\d+}' => fn () => new Descriptions\DeleteEndpoint(
+            $container->get(App\Actions\DeleteDescriptionInterface::class),
         ),
 
-        'DELETE /runs/{run_id:\d+}/publications/{pmid:\d+}/descriptions/{id:\d+}' => fn () => $endpoint(new Descriptions\DeleteEndpoint(
-            $container->get(App\Actions\DeleteDescriptionInterface::class),
-        )),
-
-        'GET /methods' => fn () => $endpoint(new Methods\IndexEndpoint(
+        'GET /methods' => fn () => new Methods\IndexEndpoint(
             $container->get(App\ReadModel\MethodViewInterface::class),
-        )),
+        ),
 
-        'GET /methods/{id:[0-9]+}' => fn () => $endpoint(new Methods\ShowEndpoint(
+        'GET /methods/{id:[0-9]+}' => fn () => new Methods\ShowEndpoint(
             $container->get(App\ReadModel\MethodViewInterface::class),
-        )),
+        ),
 
-        'GET /proteins' => fn () => $endpoint(new Proteins\IndexEndpoint(
+        'GET /proteins' => fn () => new Proteins\IndexEndpoint(
             $container->get(App\ReadModel\ProteinViewInterface::class),
-        )),
+        ),
 
-        'GET /proteins/{id:[0-9]+}' => fn () => $endpoint(new Proteins\ShowEndpoint(
+        'GET /proteins/{id:[0-9]+}' => fn () => new Proteins\ShowEndpoint(
             $container->get(App\ReadModel\ProteinViewInterface::class),
-        )),
+        ),
 
-        'GET /dataset/{type:hh|vh}' => fn () => $endpoint(new Dataset\DownloadEndpoint(
+        'GET /dataset/{type:hh|vh}' => fn () => new Dataset\DownloadEndpoint(
             $container->get(App\ReadModel\DatasetViewInterface::class),
-        )),
+        ),
 
-        'POST /jobs/alignments' => fn () => $endpoint(new Alignments\StartEndpoint(
+        'POST /jobs/alignments' => fn () => new Alignments\StartEndpoint(
             $container->get(Predis\Client::class),
-        )),
+        ),
     ]);
 };
