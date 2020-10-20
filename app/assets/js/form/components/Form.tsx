963eeb02fd7e1132c22ddc626653b6d73937eda5
pierre <pm@enyopharma.com>
2020-10-20 15:53:48 +0200

    little cleanup of main form component

--- app/assets/js/form/components/Form.tsx
+++ app/assets/js/form/components/Form.tsx
@@ -23,49 +23,7 @@ export const Form: React.FC = () => {
     const props = useAppSelector(state => state)
     const [tab, setTab] = useState<InteractorI>(1)
 
-    const { type, saving, savable, resetable, feedback } = props
-
-    const editing = props.stable_id.trim().length > 0
-
-    const onSave = () => {
-        if (editing) {
-            MySwal.fire({
-                icon: 'warning',
-                title: 'Are you sure?',
-                text: `Are you sure you want to create a new version of description ${props.stable_id}?`,
-                showCancelButton: true,
-                confirmButtonText: 'Save',
-                cancelButtonColor: '#d33',
-            }).then(result => {
-                if (result.isConfirmed) save()
-            })
-        } else {
-            save()
-        }
-    }
-
-    const onReset = () => {
-        reset()
-        setTab(1)
-        document.getElementById('form-top')?.scrollIntoView()
-        MySwal.fire({ icon: 'success', text: 'form reseted!' })
-    }
-
-    useEffect(() => {
-        if (!feedback) return
-        if (feedback.success) {
-            MySwal.fire({
-                icon: 'success',
-                text: 'Description successfully saved!',
-            })
-        } else {
-            MySwal.fire({
-                icon: 'error',
-                title: <p>Something went wrong</p>,
-                html: <ul>{feedback.errors.map((e, i) => <li key={i}>{e}</li>)}</ul>,
-            })
-        }
-    }, [feedback])
+    const { stable_id, type, saving, savable, resetable, feedback } = props
 
     return (
         <form id="form-top" onSubmit={e => e.preventDefault()}>
@@ -83,31 +41,21 @@ export const Form: React.FC = () => {
                     <MappingFieldset i={tab} />
                 </div>
                 <div className="card-footer">
-                    {editing && (
+                    {stable_id.trim().length > 0 && (
                         <div className="alert alert-danger">
                             Validating this form will create a new version of description {props.stable_id}!
                         </div>
                     )}
                     <div className="row">
                         <div className="col">
-                            <button
-                                type="button"
-                                className="btn btn-block btn-primary"
-                                onClick={() => onSave()}
-                                disabled={!savable}
-                            >
-                                <SaveIcon saving={saving} /> Save description
-                            </button>
+                            <SaveButton stable_id={stable_id} enabled={savable} saving={saving} save={save}>
+                                Save description
+                            </SaveButton>
                         </div>
                         <div className="col">
-                            <button
-                                type="button"
-                                className="btn btn-block btn-primary"
-                                onClick={() => onReset()}
-                                disabled={!resetable}
-                            >
-                                <FontAwesomeIcon icon={faEraser} /> Reset form data
-                            </button>
+                            <ResetButton enabled={resetable} reset={() => { reset(); setTab(1) }}>
+                                Reset form data
+                            </ResetButton>
                         </div>
                     </div>
                     {feedback && <FeedbackRow feedback={feedback} />}
@@ -117,11 +65,97 @@ export const Form: React.FC = () => {
     )
 }
 
-const SaveIcon: React.FC<{ saving: boolean }> = ({ saving }) => saving
-    ? <span className="spinner-border spinner-border-sm"></span>
-    : <FontAwesomeIcon icon={faSave} />
+type ButtonProps = {
+    enabled: boolean
+    onClick: () => void
+}
 
-const FeedbackRow: React.FC<{ feedback: Feedback }> = ({ feedback }) => {
+const Button: React.FC<ButtonProps> = ({ enabled, onClick, children }) => (
+    <button
+        type="button"
+        className="btn btn-block btn-primary"
+        onClick={onClick}
+        disabled={!enabled}
+    >
+        {children}
+    </button>
+)
+
+type ResetButtonProps = {
+    enabled: boolean
+    reset: () => void
+}
+
+const ResetButton: React.FC<ResetButtonProps> = ({ enabled, reset, children }) => {
+    const onClick = () => {
+        reset()
+        document.getElementById('form-top')?.scrollIntoView()
+        MySwal.fire({ icon: 'success', text: 'form reseted!' })
+    }
+
+    return (
+        <Button enabled={enabled} onClick={onClick}>
+            <FontAwesomeIcon icon={faEraser} /> {children}
+        </Button>
+    )
+}
+
+type SaveButtonProps = {
+    stable_id: string
+    enabled: boolean
+    saving: boolean
+    save: () => void
+}
+
+const SaveButton: React.FC<SaveButtonProps> = ({ stable_id, enabled, saving, save }) => {
+    const onClick = () => {
+        if (stable_id.trim().length > 0) {
+            MySwal.fire({
+                icon: 'warning',
+                title: 'Are you sure?',
+                text: `Are you sure you want to create a new version of description ${stable_id}?`,
+                showCancelButton: true,
+                confirmButtonText: 'Save',
+                cancelButtonColor: '#d33',
+            }).then(result => {
+                if (result.isConfirmed) save()
+            })
+        } else {
+            save()
+        }
+    }
+
+    const icon = saving
+        ? <span className="spinner-border spinner-border-sm"></span>
+        : <FontAwesomeIcon icon={faSave} />
+
+    return (
+        <Button enabled={enabled} onClick={onClick}>
+            {icon} Save description
+        </Button>
+    )
+}
+
+type FeedbackRowProps = {
+    feedback: Feedback
+}
+
+const FeedbackRow: React.FC<FeedbackRowProps> = ({ feedback }) => {
+    useEffect(() => {
+        if (feedback.success) {
+            MySwal.fire({
+                icon: 'success',
+                text: 'Description successfully saved!',
+            })
+        } else {
+            MySwal.fire({
+                icon: 'error',
+                title: <p>Something went wrong</p>,
+                html: <ul>{feedback.errors.map((e, i) => <li key={i}>{e}</li>)}</ul>,
+            })
+        }
+    }, [feedback])
+
     if (!feedback.success) return null
 
     return (
