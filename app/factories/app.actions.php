0bdf8fbf1f89dd09c0584a8b964bde88f12d1935
pmall <pierre.mallinjoud@gmail.com>
2022-06-28 13:32:17 +0200

    move cli to its own repo

--- app/factories/app.actions.php
+++ app/factories/app.actions.php
@@ -2,30 +2,14 @@
 
 declare(strict_types=1);
 
-use App\Actions\StoreRunInterface;
-use App\Actions\PopulateRunInterface;
 use App\Actions\StorePeptideInterface;
 use App\Actions\StoreDescriptionInterface;
 use App\Actions\DeleteDescriptionInterface;
-use App\Actions\PopulatePublicationInterface;
 use App\Actions\UpdatePublicationStateInterface;
 
 return [
-    StoreRunInterface::class => fn ($container) => new App\Actions\StoreRunSql(
+    StorePeptideInterface::class => fn ($container) => new App\Actions\StorePeptideSql(
         $container->get(PDO::class),
-    ),
-
-    PopulateRunInterface::class => fn ($container) => new App\Actions\PopulateRunSql(
-        $container->get(PDO::class),
-    ),
-
-    UpdatePublicationStateInterface::class => fn ($container) => new App\Actions\UpdatePublicationStateSql(
-        $container->get(PDO::class),
-    ),
-
-    PopulatePublicationInterface::class => fn ($container) => new App\Actions\PopulatePublicationSql(
-        $container->get(PDO::class),
-        new App\Services\Efetch,
     ),
 
     StoreDescriptionInterface::class => fn ($container) => new App\Actions\StoreDescriptionSql(
@@ -36,7 +20,7 @@ return [
         $container->get(PDO::class),
     ),
 
-    StorePeptideInterface::class => fn ($container) => new App\Actions\StorePeptideSql(
+    UpdatePublicationStateInterface::class => fn ($container) => new App\Actions\UpdatePublicationStateSql(
         $container->get(PDO::class),
     ),
 ];
